3d1825614d4f1bd4d5e4cdaacfcaebe3c29e539f
Trisha Dring <trisha.dring@gmail.com>
2021-10-08 17:20:49 -0400

    added cpanel file

--- src/components/about.js
+++ src/components/about.js
@@ -5,6 +5,16 @@ const About = () =>  {
     <div className="container" >
       <h1>About Me</h1>
 			<p class="explain">Hi. I'm Trisha</p>
+      <p>
+        I like to Code.
+        I've been working on getting a Front end Development job. 
+      </p>
+
+      <a className="nav-link" href="https://www.linkedin.com/in/trisha-dring-1488b95b/">Linkedin</a>
+      <a className="nav-link" href="https://github.com/Trishadring">GitHub</a>
+      <a className="nav-link" href="https://www.ebay.com/str/gagnierfabric">Ebay Store</a>
+      <a className="nav-link" href="https://www.etsy.com/shop/GagnierInc?ref=profile_header">Etsy</a>
+
     </div>
   );
 }
